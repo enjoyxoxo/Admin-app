3f6940689fce3b4ea412aa3e0600d432cf48f6bb
Asante <asante0277@gmail.com>
2023-02-22 09:23:08 -0800

    MainLayout, Dashboard Component All Updated

--- src/Components/MainLayout.js
+++ src/Components/MainLayout.js
@@ -3,11 +3,21 @@ import './MainLayout.css';
 import {
   MenuFoldOutlined,
   MenuUnfoldOutlined,
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
 } from '@ant-design/icons';
+import { 
+  AiOutlineDashboard, 
+  AiOutlineShoppingCart,
+  AiOutlineUser,
+  AiOutlineBgColors,
+} from 'react-icons/ai';
+import { Outlet } from 'react-router-dom';
+import { SiBrandfolder } from 'react-icons/si';
+import { BiCategoryAlt } from 'react-icons/bi';
+import { FaClipboardList, FaBloggerB } from 'react-icons/fa';
+import { ImBlog } from 'react-icons/im';
+import { IoIosNotifications } from 'react-icons/io';
 import { Layout, Menu, theme } from 'antd';
+import { useNavigate } from 'react-router-dom';
 const { Header, Sider, Content } = Layout;
 
 const MainLayout = () => {
@@ -15,36 +25,127 @@ const MainLayout = () => {
   const {
     token: { colorBgContainer },
   } = theme.useToken();
-  
+  const navigate = useNavigate();
   return (
     <Layout>
       <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="logo" />
+        <div className="logo">
+          <h2 className='text-white fs-5 text-center py-1 mb-0'>
+            <span className='sm-logo'>ME</span>
+            <span className='lg-logo'>Medu Express</span>
+          </h2>
+        </div>
         <Menu
           theme="dark"
           mode="inline"
-          defaultSelectedKeys={['1']}
+          defaultSelectedKeys={['']}
+          onClick= {({ key }) => {
+             if (key == 'signout') {
+             } else {
+               navigate(key);
+             }
+          }}
           items={[
             {
-              key: '1',
-              icon: <UserOutlined />,
-              label: 'nav 1',
+              key: '',
+              icon: <AiOutlineDashboard className='fs-4' />,
+              label: 'Dashboard',
             },
             {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'nav 2',
+              key: 'customers',
+              icon: <AiOutlineUser className='fs-4' />,
+              label: 'Customers',
             },
             {
-              key: '3',
-              icon: <UploadOutlined />,
-              label: 'nav 3',
+              key: 'Catalog',
+              icon: <AiOutlineShoppingCart className='fs-4' />,
+              label: 'Catelog',
+              children: [
+                {
+                  key: "Product",
+                  icon: <AiOutlineShoppingCart className='fs-4' />,
+                  label: "Add Product",
+                },
+                {
+                  key: "Product-list",
+                  icon: <AiOutlineShoppingCart className='fs-4' />,
+                  label: "Product List",
+                },
+                {
+                  key: "brand",
+                  icon: <SiBrandfolder className='fs-4' />,
+                  label: "Brand",
+                },
+                {
+                  key: "list-brand",
+                  icon: <SiBrandfolder className='fs-4' />,
+                  label: "Brand List",
+                },
+                {
+                  key: "category",
+                  icon: <BiCategoryAlt className='fs-4' />,
+                  label: "Category",
+                },
+                {
+                  key: "list-category",
+                  icon: <BiCategoryAlt className='fs-4' />,
+                  label: "Category List",
+                },
+                {
+                  key: "color",
+                  icon: <AiOutlineBgColors className='fs-4' />,
+                  label: "Color",
+                },
+                {
+                  key: "list-color",
+                  icon: <AiOutlineBgColors className='fs-4' />,
+                  label: "Color List",
+                }
+              ]
+            },
+            {
+              key: 'orders',
+              icon: <FaClipboardList className='fs-4' />,
+              label: 'Orders',
+            },
+            {
+              key: 'blog',
+              icon: <FaBloggerB className='fs-4' />,
+              label: 'Blogs',
+              children: [
+                {
+                  key: 'add-blog',
+                  icon: <ImBlog className='fs-4' />,
+                  label: 'Add Blogs',
+                },
+                {
+                  key: 'blog-list',
+                  icon: <FaBloggerB className='fs-4' />,
+                  label: 'Blogs List',
+                },
+                {
+                  key: 'blog-category',
+                  icon: <ImBlog className='fs-4' />,
+                  label: 'Add Blog Category',
+                },
+                {
+                  key: 'blog-category-list',
+                  icon: <FaBloggerB className='fs-4' />,
+                  label: 'Add Blog Category List',
+                }
+              ]
+            },
+            {
+              key: 'enquiries',
+              icon: <FaClipboardList className='fs-4' />,
+              label: 'Enquiries',
             },
           ]}
         />
       </Sider>
       <Layout className="site-layout">
         <Header
+          className='d-flex justify-content-between ps-1 pe-5'
           style={{
             padding: 0,
             background: colorBgContainer,
@@ -54,6 +155,23 @@ const MainLayout = () => {
             className: 'trigger',
             onClick: () => setCollapsed(!collapsed),
           })}
+          <div className='d-flex gap-4 align-items-center'>
+            <div className='position-relative'>
+              <IoIosNotifications className='fs-4' />
+              <span className='badge bg-warning rounded-circle p-1 position-absolute'>3</span>
+            </div>
+            <div className='d-flex gap-3 align-items-center'>
+              <div>
+                <img width={32}
+                  height={32}
+                  src="https://stroyka-admin.html.themeforest.scompiler.ru/variants/ltr/images/customers/customer-4-64x64.jpg" alt="" />
+              </div>
+              <div>
+                <h5 className='mb-0'>Meduexpress</h5>
+                <p className='mb-0'>palito0277@gmail.com</p>
+              </div>
+            </div>
+          </div>
         </Header>
         <Content
           style={{
@@ -63,7 +181,7 @@ const MainLayout = () => {
             background: colorBgContainer,
           }}
         >
-          Content
+        <Outlet />
         </Content>
       </Layout>
     </Layout>
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,128 @@
 import React from 'react';
 import './Dashboard.css';
+import { BsArrowDownRight, BsArrowUpRight } from 'react-icons/bs';
+import { Column } from '@ant-design/plots';
+
 
 const Dashboard = () => {
+  const data = [
+    {
+      type: 'Jan',
+      sales: 38,
+    },
+    {
+      type: 'Feb',
+      sales: 52,
+    },
+    {
+      type: 'Mar',
+      sales: 61,
+    },
+    {
+      type: 'April',
+      sales: 145,
+    },
+    {
+      type: 'May',
+      sales: 48,
+    },
+    {
+      type: 'June',
+      sales: 38,
+    },
+    {
+      type: 'July',
+      sales: 38,
+    },
+    {
+      type: 'Aug',
+      sales: 38,
+    },
+    {
+      type: 'Sep',
+      sales: 38,
+    },
+    {
+      type: 'Oct',
+      sales: 38,
+    },
+    {
+      type: 'Nov',
+      sales: 38,
+    },
+    {
+      type: 'Dec',
+      sales: 38,
+    },
+  ];
+  const config = {
+    data,
+    xField: 'type',
+    yField: 'sales',
+    color: ({ type }) => {
+      return '#0d6efd';
+    },
+    label: {
+      position: 'middle',
+      style: {
+        fill: '#FFFFFF',
+        opacity: 1,
+      },
+    },
+    xAxis: {
+      label: {
+        autoHide: true,
+        autoRotate: false,
+      },
+    },
+    meta: {
+      type: {
+        alias: 'Month',
+      },
+      sales: {
+        alias: 'Income',
+      },
+    },
+  };
   return <>
-  
+   <div>
+     <h3 className='mb-4'>Dashboard</h3>
+     <div className='d-flex justify-content-between align-items-center gap-3'>
+       <div className='d-flex justify-content-between align-items-end flex-grow-1 bg-white p-3 rounded-3'>
+         <div>
+           <p className=''>Total</p> <h4 className='mb-0'>$1100</h4>
+         </div>
+         <div className='d-flex flex-column align-items-end'>
+           <h6><BsArrowDownRight />32%</h6>
+           <p className='mb-0'>Compared To April 2022</p>
+         </div>
+       </div>
+       <div className='d-flex justify-content-between align-items-end flex-grow-1 bg-white p-3 rounded-3'>
+         <div>
+           <p className=''>Total</p> <h4 className='mb-0'>$1100</h4>
+         </div>
+         <div className='d-flex flex-column align-items-end'>
+           <h6 className='red'><BsArrowDownRight />32%</h6>
+           <p className='mb-0'>Compared To April 2022</p>
+         </div>
+       </div>
+       <div className='d-flex justify-content-between align-items-end flex-grow-1 bg-white p-3 rounded-3'>
+         <div>
+           <p className=''>Total</p> <h4 className='mb-0'>$1100</h4>
+         </div>
+         <div className='d-flex flex-column align-items-end'>
+           <h6 className='green'><BsArrowDownRight />32%</h6>
+           <p className='mb-0'>Compared To April 2022</p>
+         </div>
+       </div>
+     </div>
+     <div className='mt-4'>
+       <h3 className='mb-4'>Income Statics</h3>
+       <div>
+         return <Column {...config} />;
+       </div>
+     </div>
+   </div>
   </>
 }
 
